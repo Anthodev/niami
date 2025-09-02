93ad63f9c4cff09662c072b658203ac8580497a8
anthodev <git@antho.dev>
2025-09-02 17:59:13 +0200

    :globe_with_meridians: (i18n): Added symfony/translation + translations in templates

--- src/Presentation/Form/SearchGameForm.php
+++ src/Presentation/Form/SearchGameForm.php
@@ -8,26 +8,39 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SearchGameForm extends AbstractType
 {
+    public function __construct(
+        private readonly TranslatorInterface $translator,
+    ) {}
+
     public function buildForm(
         FormBuilderInterface $builder,
         array $options,
     ): void {
         $builder->add('game', TextType::class, [
-            'label' => 'Search the game:',
+            'label' => $this->translator->trans('form.label', [], 'search'),
             'required' => false,
             'constraints' => [
                 new Assert\Length(
                     min: 3,
                     max: 100,
-                    minMessage: 'The search query must be at least {{ limit }} characters.',
-                    maxMessage: 'The search query can\'t be above {{ limit }} characters.',
+                    minMessage: $this->translator->trans(
+                        'form.constraint.min_length_message',
+                        ['limit' => '{{ limit }}'],
+                        'search',
+                    ),
+                    maxMessage: $this->translator->trans(
+                        'form.constraint.max_length_message',
+                        ['limit' => '{{ limit }}'],
+                        'search',
+                    ),
                 ),
             ],
             'attr' => [
-                'placeholder' => 'Type at least 3 characters...',
+                'placeholder' => $this->translator->trans('form.placeholder', [], 'search'),
                 'autocomplete' => 'off',
             ],
         ]);
